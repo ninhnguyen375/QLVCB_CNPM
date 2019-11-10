f122e96372021ab2f201fe5583e6900934b1216f
Sang <superseasa1@gmail.com>
2019-11-10 14:40:40 +0700

    Update full api for Airlines

---
+++ webapi.core/UseCases/SearchAirline.cs
@@ -0,0 +1,10 @@
+namespace webapi.core.UseCases
+{
+    public class SearchAirline
+    {
+      public string Id { get; set; } = "";
+      public string Name { get; set; } = "";
+      public string sortAsc { get; set; } = "";
+      public string sortDesc { get; set; } = "";
+    }
+}
