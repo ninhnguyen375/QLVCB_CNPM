08f0f4b59dc9b05227feadcb3e22040e7599710f
Ninh <ninhnguyen375@gmail.com>
2019-11-10 23:58:04 +0700

    Update Edit user

--- webapi.core/UseCases/EditUser.cs
+++ webapi.core/UseCases/EditUser.cs
@@ -1,5 +1,9 @@
 namespace webapi.core.UseCases {
     public class EditUser {
-        public string email { get; set; }
+        public string email { get; set; } = "";
+        public string phone { get; set; } = "";
+        public string identifier { get; set; } = "";
+        public string fullName { get; set; } = "";
+        public string password { get; set; } = "";
     }
 }
