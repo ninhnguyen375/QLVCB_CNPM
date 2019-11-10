6d7a5d1d49c759203e1ded47e187ad807e6d6562
Sang <superseasa1@gmail.com>
2019-11-10 21:13:45 +0700

    Update full api for Customers

---
+++ webapi.core/UseCases/SearchCustomer.cs
@@ -0,0 +1,11 @@
+namespace webapi.core.UseCases
+{
+    public class SearchCustomer
+    {
+        public string Id { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string FullName { get; set; } = "";
+        public string sortAsc { get; set; } = "";
+        public string sortDesc { get; set; } = "";
+    }
+}
