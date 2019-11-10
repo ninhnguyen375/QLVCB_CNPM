1d687ff5f2976c0f78ad2469e861a7983b21b885
Sang <superseasa1@gmail.com>
2019-11-10 12:25:50 +0700

    Update full api for TicketCategories

---
+++ webapi.core/UseCases/SearchTicketCategory.cs
@@ -0,0 +1,9 @@
+namespace webapi.core.UseCases
+{
+    public class SearchTicketCategory
+    {
+        public string Name { get; set; } = "";
+        public string sortAsc { get; set; } = "";
+        public string sortDesc { get; set; } = "";
+    }
+}
