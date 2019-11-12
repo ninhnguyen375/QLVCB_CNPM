731e64c375333382419122c4981839191ec8f9fd
Ninh <ninhnguyen375@gmail.com>
2019-11-13 01:08:20 +0700

    Improve FlightController

--- webapi.core/Interfaces/IFlightRepository.cs
+++ webapi.core/Interfaces/IFlightRepository.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using webapi.core.Domain.Entities;
 
 namespace webapi.core.Interfaces
 {
     public interface IFlightRepository : IRepository<Flight>
-    {       
+    {  
+        public IEnumerable<FlightTicketCategory> GetFlightTicketCategories ();     
     }
 }
