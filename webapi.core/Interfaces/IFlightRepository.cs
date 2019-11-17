459533f702f739fd918e497bacb6bc405bec9c0f
Sang <superseasa1@gmail.com>
2019-11-17 13:44:41 +0700

    Addition: Put Flight, Add + Remove FlightTicketCategory for Flights

--- webapi.core/Interfaces/IFlightRepository.cs
+++ webapi.core/Interfaces/IFlightRepository.cs
@@ -5,6 +5,8 @@ namespace webapi.core.Interfaces
 {
     public interface IFlightRepository : IRepository<Flight>
     {  
-        public IEnumerable<FlightTicketCategory> GetFlightTicketCategories ();     
+        public IEnumerable<FlightTicketCategory> GetFlightTicketCategories ();
+
+        public IEnumerable<FlightTicketCategory> GetFlightTicketCategoriesById (string id);    
     }
 }
