59944b4ffe8cc3ec1260c129801aa54d48e894f6
Sang <superseasa1@gmail.com>
2019-12-10 18:52:13 +0700

    Update Test

--- Src/webapi.core/DTOs/SaveCustomerDTO.cs
+++ Src/webapi.core/DTOs/SaveCustomerDTO.cs
@@ -5,14 +5,12 @@ namespace webapi.core.DTOs
     public class SaveCustomerDTO
     {
         [Required(ErrorMessage = "CMND không được để trống.")]
-        // Thiếu RegEx
         public string Id { get; set; }
 
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Họ tên phải có ít nhất 2 ký tự.")]
         public string FullName { get; set; }
 
         [StringLength(10, MinimumLength = 10, ErrorMessage = "Số điện thoại không chính xác.")]
-        // Thiếu RegEx
         public string Phone { get; set; }
     }
 }
